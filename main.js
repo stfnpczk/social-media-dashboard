989aa7c3666c3e2382f828b13318663659a56a7e
StefanP <stefandev_mail@pm.me>
2022-07-16 14:59:25 +0200

    Fix html attribute issue

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 const toggleSwitch = document.getElementById("switch");
+const colorTheme = document.querySelector(".dark-theme");
 
 const toggleColorTheme = (e) => {
   toggleSwitch.classList.toggle("switch-off");
 
   if (e.currentTarget.classList.contains("switch-off")) {
-    document.documentElement.setAttribute("color-theme", "light");
-    localStorage.setItem("color-theme", "light");
+    document.body.className = "light-theme";
   } else {
-    document.documentElement.setAttribute("color-theme", "dark");
-    localStorage.setItem("color-theme", "dark");
+    document.body.className = "dark-theme";
   }
 };
 
